daf67c891eec6a30f315f46ac121b585185daf55
Astr0clad <colin.montigel@gmail.com>
2023-02-22 00:35:36 -0700

    UI?

--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -1,4 +1,5 @@
 const { invoke } = window.__TAURI__.tauri;
+const { invoke } = require('@tauri-apps/api/tauri')
 
 let greetInputEl;
 let greetMsgEl;
@@ -15,3 +16,9 @@ window.addEventListener("DOMContentLoaded", () => {
     .querySelector("#greet-button")
     .addEventListener("click", () => greet());
 });
+
+// Define an endpoint that calls the `register_user` function in your Rust backend
+async function registerUser(permanentLogin, displayName, password) {
+  const result = await invoke('register_user', permanentLogin, displayName, password)
+  return result
+}
