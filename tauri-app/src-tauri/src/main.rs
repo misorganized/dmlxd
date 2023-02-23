4b2ebfe24630cda6c3725c38f39b2b7be001a9ed
David <daviru007@icloud.com>
2023-02-22 23:15:27 -0800

    quick fixups, added logo, no errors, added return button for the signup page

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -5,8 +5,9 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn greet(i: i32) -> String
+{
+    format!("{}", i)
 }
 
 fn main() {
--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -1,24 +1,7 @@
 const { invoke } = window.__TAURI__.tauri;
-const { invoke } = require('@tauri-apps/api/tauri')
+//const { invoke } = require('@tauri-apps/api/tauri')
 
-let greetInputEl;
-let greetMsgEl;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
-});
-
-// Define an endpoint that calls the `register_user` function in your Rust backend
-async function registerUser(permanentLogin, displayName, password) {
-  const result = await invoke('register_user', permanentLogin, displayName, password)
-  return result
-}
+const title = document.querySelector('.title');
+const chars = title.textContent.split('');
+title.innerHTML = chars.map(c => `<span>${c}</span>`).join('');
